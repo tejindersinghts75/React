3ff7e9d7d467e0d35aa618db9efd193d2e21327d
tejindersinghts75 <tejindersinghts75@gmail.com>
2024-05-04 21:22:32 +0530

    instapost

--- src/SocialMedia/Componenets/InstagramPost.js
+++ src/SocialMedia/Componenets/InstagramPost.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Button } from 'react-bootstrap';
+import { auth, db } from "../../Firebase"
+import { push,ref } from "firebase/database";
 
 const PostCard = ({ image, username, text, likes, comments }) => {
+  const [ value, setValue] = useState({post:''})
+  const handleChangel=(e)=>{
+    setValue({...value, [e.target.name]: e.target.value})
+  }
+  console.log(value)
+  const addPost=(e)=>{
+    e.preventDefault()
+  try {
+    const dbRef = ref(db, 'posts')
+    push(dbRef , value)
+  } catch (error) {
+    console.log(error)
+  }
+}
   return (
     <div>
         <div>
-          <input type='text' placeholder='Whats in your mind'/>
+          <form>
+          <input type='text' name='post' placeholder='Whats in your mind' onChange={handleChangel}/>
+          {/* <input type='file' name='image' placeholder='Upload Image' onChange={handleChangel}/> */}
+          <button onClick={addPost}>Post</button>
+          </form>
         </div>
 
     </div>
